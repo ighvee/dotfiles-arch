2d2f44cf8148cb2da3f62e288f85c3b1d9fbdd61
ivy <mystivinewyvern@gmail.com>
2026-08-10 18:17:36 -0500

    added install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-sudo pacman -S --needed waybar rofi kitty fastfetch btop mako thunar python-pywal easyeffects firefox && yay -S --needed snappy-switcher oh-my-posh nvibrant vscodium vesktop
+#!/bin/bash
+
+sudo pacman -S --needed waybar rofi kitty fastfetch btop mako thunar python-pywal easyeffects firefox
+
+yay -S --needed snappy-switcher oh-my-posh nvibrant vscodium vesktop
