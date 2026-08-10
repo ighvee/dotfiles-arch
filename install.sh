270602003fea55a4ef47d8eba13778a6eebb924e
ivy <mystivinewyvern@gmail.com>
2026-08-10 18:23:43 -0500

    added install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
-sudo pacman -S --needed waybar rofi kitty fastfetch btop mako thunar python-pywal easyeffects firefox
+sudo pacman -S --needed \
+    waybar \
+    rofi-wayland \
+    kitty \
+    fastfetch \
+    btop \
+    mako \
+    thunar \
+    thunar-archive-plugin \
+    thunar-volman \
+    gvfs \
+    gvfs-mtp \
+    gvfs-smb \
+    xdg-utils \
+    xdg-user-dirs \
+    xdg-desktop-portal \
+    xdg-desktop-portal-hyprland \
+    file-roller \
+    p7zip \
+    unzip \
+    zip \
+    unrar \
+    python-pywal \
+    easyeffects \
+    pipewire \
+    pipewire-pulse \
+    wireplumber \
+    libnotify \
+    playerctl \
+    firefox \
+    ttf-jetbrains-mono-nerd \
+    ttf-font-awesome \
+    noto-fonts \
+    noto-fonts-emoji
 
-yay -S --needed snappy-switcher oh-my-posh nvibrant vscodium vesktop
+yay -S --needed \
+    snappy-switcher \
+    oh-my-posh \
+    nvibrant \
+    vscodium \
+    vesktop
